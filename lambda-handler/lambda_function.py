b1754b576ec39a413197901965a014a043bd55d8
Ujwal budha <ujjwal.budha07@gmail.com>
2024-07-11 23:36:35 +0545

    updated the lambda function

--- lambda-handler/lambda_function.py
+++ lambda-handler/lambda_function.py
@@ -3,26 +3,14 @@ import boto3
 import os
 
 def lambda_handler(event, context):
-    s3_event = event['Records'][0]
-    
-    s3_bucket = s3_event['s3']['bucket']['name']
-    s3_object_key = s3_event['s3']['object']['key']
-    
-    print("S3 Bucket:", s3_bucket)
-    print("S3 Object Key:", s3_object_key)
-    
-    notification = f"Hello, something has been added into the {s3_bucket} bucket. Object key: {s3_object_key}."
+    notification = "Hello, something has been added into the bucket."
     print(notification)
     
     client = boto3.client('sns')
     
     response = client.publish(
         TopicArn=os.environ['SNS_TOPIC_ARN'],
-        Message=json.dumps({'default': notification}),
-        MessageStructure='json'
+        Message=notification
     )
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response)
-    }
+    print("Message published")
+    return response
